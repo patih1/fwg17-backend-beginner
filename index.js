6900b38229d53ecda6e7f34918cda63ccbb1958f
patih1 <haidar4941@gmail.com>
2024-04-15 06:57:33 +0700

    cleanup ndex

--- index.js
+++ index.js
@@ -1,38 +1,27 @@
-// require = digunakan untuk menghubungkan sebuah file dengan file lainnya menggunakan path
-// ketika jalur telah dihubungkan, data/program yang di export dari file sumber, dapat digunakan dalam file tujuan
-
 require('dotenv').config({
   path: './.env'
 })
 
 global.path = __dirname
 
-// memanggil framework express dan disimpan dalam variable express agar dapat digunakan dalam program
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 
-// mendeklarasikan app sebagai fungsi express yang dipanggil
 const app = express()
+app.use(morgan('dev'))
 app.use(express.json())
 app.use('/uploads/products',express.static('upload/products'))
 app.use('/uploads/users',express.static('upload/users'))
 
-// app.use = fungsi middleware bersisi callback fungsi yang bersumber dari path spesifik. akan berjalan jika path sesuai dengan sumber fungsi
-
-// fungsi untuk menguraikan request dengan urlencoded dalam body
 app.use(express.urlencoded({extended: true}))
 var corsOptions = {
-  // origin: 'http://localhost:5173'
   origin: process.env.ALLOWED_ORIGIN
 }
 app.use(cors(corsOptions))
 
-// menghubungkan file dengan file index router pada folder src/routers sekaligus menambahkan endpoint /
 app.use('/', require('./src/routers'))
 
-// mengambil data dalam bentuk json pada endpoint /
-// program digunakan untuk memastikan bahwa url sudah sesuai
 app.get('/', (req, res) => {
   return res.json ({
     success: true,
@@ -40,7 +29,6 @@ app.get('/', (req, res) => {
   })
 })
 
-// menjalankan program pada port 5050
 app.listen(process.env.PORT, ()=>{
   console.log(`App listening on port ${process.env.PORT}`)
 })
